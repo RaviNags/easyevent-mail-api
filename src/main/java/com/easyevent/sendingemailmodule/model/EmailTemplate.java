82cb134f3b264020016b44edab6938d96932fb89
Ravi <ravi.nagalingam@preconys.com>
2020-06-16 11:55:44 +0200

    Feature: add reza key

--- src/main/java/com/easyevent/sendingemailmodule/model/EmailTemplate.java
+++ src/main/java/com/easyevent/sendingemailmodule/model/EmailTemplate.java
@@ -4,13 +4,13 @@ public enum EmailTemplate {
 
     HEBDO_SITUATION(1),
     REGISTER(3),
-    COLLABORATOR_INVITATION(4);
+    COLLABORATOR_INVITATION(4),
+    RESA_CONTACT(6);
 
     private final int templateId;
 
     EmailTemplate(int templateId) {
         this.templateId = templateId;
-
     }
 
     public int getTemplateId() {
